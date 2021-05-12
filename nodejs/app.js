545268c6381a79ebe9da05158678ef463d4860d8
websiteppages <53472518+websiteppages@users.noreply.github.com>
2021-05-12 15:47:48 +0530

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,1 +1,115 @@
-const express = require('express')const app = express()const path = require('path');const cors = require('cors');const bodyParser = require('body-parser');const allRoutes = require("./routes");const port = process.env.PORT || 8080;const fs = require('fs');const db_connection = require("./config/database");// ------- https redirect code -------------    app.enable('trust proxy');    app.use (function (req, res, next) {            if (req.secure) {                    // request was via https, so do no special handling                    // return next();                    next();            } else {                    // request was via http, so redirect to https                    res.redirect('https://15.207.42.2:3000' + req.url);            }    });//-----------------------------------------------------------------------    app.use(cors())    app.use(bodyParser.json() );      app.use(bodyParser.urlencoded({extended:true}));// ------------------ React redirect code -------------------------   app.use(express.static(path.join(__dirname, '../public_html/build')));    app.get('*', function(req, res) {    	res.sendFile(path.join(__dirname, '../public_html/build', 'index.html'));   });   // Handling Errorsapp.use((err, req, res, next) => {  // console.log(err);  err.statusCode = err.statusCode || 500;  err.message = err.message || "Internal Server Error";  res.status(err.statusCode).json({    message: err.message,  });});//---------------- Folder Path --------------------------    app.get('/ping', function (req, res) {     return res.send(path.join(__dirname));    });//-----------------------------------------------------------------app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const express = require('express')
+const app = express()
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080;
+const fs = require('fs');
+
+
+
+// ------- https redirect code -------------
+
+
+//-----------------------------------------------------------------------
+
+
+
+    app.use(cors())
+
+
+
+    app.use(bodyParser.json() );  
+
+
+
+    app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+// ------------------ React redirect code -------------------------
+
+
+
+   app.use(express.static(path.join(__dirname, '../public_html/build')));
+
+    app.get('*', function(req, res) {
+
+    	res.sendFile(path.join(__dirname, '../public_html/build', 'index.html'));
+
+   });
+
+   
+
+
+
+// Handling Errors
+
+
+
+app.use((err, req, res, next) => {
+
+
+
+  // console.log(err);
+
+
+
+  err.statusCode = err.statusCode || 500;
+
+
+
+  err.message = err.message || "Internal Server Error";
+
+
+
+  res.status(err.statusCode).json({
+
+
+
+    message: err.message,
+
+
+
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+//---------------- Folder Path --------------------------
+
+
+
+    app.get('/ping', function (req, res) {
+
+
+
+     return res.send(path.join(__dirname));
+
+
+
+    });
+
+
+
+
+
+
+
+//-----------------------------------------------------------------
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
